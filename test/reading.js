bc32ed21926c3a5cb2ccf6df5d4f9f5f7b9d86c2
Percy Bolmer <percybolmer1@gmail.com>
2022-06-19 23:45:42 +0200

    started first test

---
+++ test/reading.js
@@ -0,0 +1,12 @@
+const Counter = artifacts.require("Counter");
+
+contract("Counter", (accounts) => {
+  it("balanceOf Address", async () => {
+    const counterInstance = await Counter.deployed();
+    console.log(accounts[0]);
+    const balance = await counterInstance.getCount.call(accounts[0]);
+
+    assert.equal(balance.valueOf(), 100, "100 wasn't in the first account");
+  });
+
+});
